bb95f85d5c52e8c007a751bf71866c7b32c2e368
dddingnan <silenceapple1027@gmail.com>
2023-06-16 18:23:20 -0400

    feat: transform composer write as vue js.

---
+++ public/javascript/composer/chopin.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      composer: {
+        Biography: null,
+        Creations: [],
+        MusicalStyles: null,
+        FavoriteComposition: { name: null, audioSource: null },
+        Trivia: null,
+        InfluencesAndLegacy: { text: null, imgSrc: null },
+      },
+    };
+  },
+  mounted() {
+    fetch("../static/data/composer/chopin.json")
+      .then((response) => response.json())
+      .then((data) => (this.composer = data));
+  },
+});
+app.mount("#app");
